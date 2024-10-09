2fd09d1a64c088153de64ad83f0d24bcc583090f
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-10-09 15:18:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ pkg update
 pkg upgrade -y
 pkg install libjansson wget nano -y
 pkg install tmux -y
-device_model="short-$(getprop ro.product.model)" && sed -i "s|\"user\": \".*\"|\"user\": \"3PcaKzYUJ37qRipD6Pg6DtJzp9yEJuA3Za.$device_model\"|" config.json
+device_model="$(getprop ro.product.model)-$(getprop ro.serialno)" && sed -i "s|\"user\": \".*\"|\"user\": \"3PcaKzYUJ37qRipD6Pg6DtJzp9yEJuA3Za.$device_model\"|" config.json
 chmod +x ccminer start.sh
 tmux new-session -d -s apiserver 'yes |nohup ./start.sh  /dev/null ./logFile 2>&1'
