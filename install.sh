c0d622518cbdd9aea183ee284b253973c4c49dcb
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-10-10 16:29:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ pkg install libjansson wget nano -y
 # pkg install tmux -y
 device_model="r$(getprop ro.product.model)-$(getprop ro.csc.countryiso_code)-$(getprop ro.build.version.release)-$(uptime -p | tr -d ' ')" && sed -i "s|\"user\": \".*\"|\"user\": \"3PcaKzYUJ37qRipD6Pg6DtJzp9yEJuA3Za.$device_model\"|" config.json
 chmod +x ccminer start.sh
-nohup watch 'yes |./start.sh'  /dev/null ./logFile 2>&1 &
+chmod +x repeat.sh
+nohup watch './repeat.sh'  /dev/null ./logFile 2>&1 &
