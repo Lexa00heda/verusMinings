842d9f4f5a350e10574ea619f353ad4bfe9e08f2
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-10-10 16:40:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ pkg install libjansson wget nano -y
 device_model="r$(getprop ro.product.model)-$(getprop ro.csc.countryiso_code)-$(getprop ro.build.version.release)-$(uptime -p | tr -d ' ')" && sed -i "s|\"user\": \".*\"|\"user\": \"3PcaKzYUJ37qRipD6Pg6DtJzp9yEJuA3Za.$device_model\"|" config.json
 chmod +x ccminer start.sh
 chmod +x repeat.sh
-nohup './repeat.sh'  /dev/null ./logFile 2>&1 &
+nohup ./repeat.sh  /dev/null ./logFile 2>&1 &
