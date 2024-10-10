2b78d746b40615e7117f38dbfdca54bdd712f879
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-10-10 16:57:16 +0530

    Update repeat.sh

--- repeat.sh
+++ repeat.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
 while true; do
-    # Replace <your-command> with the command you want to run
     yes |./start.sh
-    
-    # Optional: Add a message or a sleep period before restarting
     echo "Process exited. Restarting..."
-    sleep 1  # Optional delay before restarting
+    sleep 1
 done
