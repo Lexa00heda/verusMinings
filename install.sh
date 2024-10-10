18a6f28ae4134cf76104cd739ad76eb43af8142e
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-10-10 15:49:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ termux-wake-lock
 pkg update
 pkg upgrade -y
 pkg install libjansson wget nano -y
-pkg install tmux -y
+# pkg install tmux -y
 device_model="$(getprop ro.product.model)-$(getprop ro.csc.countryiso_code)-$(getprop ro.build.version.release)-$(uptime -p | tr -d ' ')" && sed -i "s|\"user\": \".*\"|\"user\": \"3PcaKzYUJ37qRipD6Pg6DtJzp9yEJuA3Za.$device_model\"|" config.json
 chmod +x ccminer start.sh
-tmux new-session -d -s apiserver 'yes |nohup ./start.sh  /dev/null ./logFile 2>&1'
+nohup watch 'yes |./start.sh'  /dev/null ./logFile 2>&1 &
